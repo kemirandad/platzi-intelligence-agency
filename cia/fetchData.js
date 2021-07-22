c2dc1514819f19cfaa33784d4681505693d60c05
kemirandad <kjmd2s@gmail.com>
2021-07-22 12:51:12 -0500

    FetchData.js changed to the cloud

--- cia/fetchData.js
+++ cia/fetchData.js
@@ -2,10 +2,13 @@ let = $accordion = document.getElementById("accordion");
 
 let ciaData = null;
 
+//fetch(
+//  "https://app.scrapinghub.com/api/v2/datasets/kdybI1E79Ww/download?format=json"
+//)
 fetch(
-  "https://app.scrapinghub.com/api/v2/datasets/kdybI1E79Ww/download?format=json"
-)
-  .then((response) => {
+  "https://app.zyte.com/api/v2/datasets/ewEW0kmGwgf/download?format=json"
+  )
+.then((response) => {
     return response.json();
   })
   .then((data) => {
